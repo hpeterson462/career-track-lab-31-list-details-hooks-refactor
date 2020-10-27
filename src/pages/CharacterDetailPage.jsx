bf9e65e831e51cbe1bae04fc0e72493c76137d6d
Heather Peterson <hpeterson462@gmail.com>
2020-10-27 15:37:39 -0700

    character detail page built

---
+++ src/pages/CharacterDetailPage.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import CharacterDetail from '../components/app/characters/CharacterDetail';
+import CharacterDetailPage from '../components/app/characters/CharacterDetail';
+
+const CharacterDetailPage = () => (
+  <CharacterDetail />
+);
+
+export default CharacterDetailPage;
