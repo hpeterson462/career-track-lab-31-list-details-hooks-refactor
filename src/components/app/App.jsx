5467267ccfcb85df3a69cc41b1e45eccb94c9c1c
Heather Peterson <hpeterson462@gmail.com>
2020-10-27 15:35:12 -0700

    router built

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,5 +1,17 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from 'react-router-dom';
+import CharacterDetail from '../app/characters/CharacterDetail';
 
 export default function App() {
-  return <h1>Hello World</h1>;
+  return (
+    <Router>
+      <Switch>
+        <Route exact path="/characters/:id" component={CharacterDetail} />
+      </Switch>
+    </Router>
+  );
 }
