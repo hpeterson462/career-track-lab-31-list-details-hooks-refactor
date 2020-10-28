35d16691c964d0fc320663530b0f74a9aaff4195
Heather Peterson <hpeterson462@gmail.com>
2020-10-27 17:21:26 -0700

    error handling in api call and pagination

--- src/services/api.js
+++ src/services/api.js
@@ -1,11 +1,26 @@
-export const getApi = (page = 1) => {
-  return fetch('https://xfiles-api.herokuapp.com/api/v1/characters')
-    .then(res => res.json())
-    .then(json => json.results);
+export const getApi = async (page = 1) => {
+  const res = await fetch(`https://xfiles-api.herokuapp.com/api/v1/characters?perPage=10&page=${page}`);
+  const json = await res.json();
+
+  if (!res.ok) throw 'Unable to find';
+
+  return json.results.map(character => ({
+    name: character.name,
+    image: character.image
+  }));
 };
 
-export const getApiByName = (name) => {
-  return fetch(`https://xfiles-api.herokuapp.com/api/v1/characters/${name}`)
-    .then(res => res.json())
-    .then(json => json.results);
+export const getApiByName = async (name) => {
+  const res = await fetch(`https://xfiles-api.herokuapp.com/api/v1/characters/${name}`)
+  const json = await res.json();
+
+  if (!res.ok) throw 'Unable to find';
+
+  return {
+    name: json.name,
+    gender: json.gender,
+    occupation: json.occupation,
+    image: json.image,
+    description: json.description
+  };
 };
